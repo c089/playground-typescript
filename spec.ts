f6263a8be793c87aad0a4e9d5c4fb2fc6323eb93
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-11-08 12:57:37 +0100

    Red: template kata

--- spec.ts
+++ spec.ts
@@ -1,3 +1,14 @@
-test('first test', () => {
-    expect(true).toBe(true);
+describe('The Template Kata', () => {
+    describe('Template',  () => {
+        it('renders a plain text template', () => {
+            class Template {
+                render(): String {
+                    return null;
+                }
+            }
+            const template = new Template();
+
+            expect(template.render()).toBe('plain text');
+        });
+    });
 });
