5a919255c51f74fb124b264f5b786b82a067f85d
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-11-08 13:03:23 +0100

    Red: test with variable

--- spec.ts
+++ spec.ts
@@ -1,14 +1,30 @@
 describe('The Template Kata', () => {
     describe('Template',  () => {
         class Template {
+            constructor(template) {
+                
+            }
+
+            set(name, value) {
+                
+            }
+
             render(): String {
                 return 'plain text';
             }
         }
         it('renders a plain text template', () => {
-            const template = new Template();
+            const template = new Template('plain text');
 
             expect(template.render()).toBe('plain text');
+        });
+
+        it('replaces a variable', () => {
+            const template = new Template('Hello, {$name}');
+
+            template.set('name', 'Chris');
+
+            expect(template.render()).toBe('Hello, Chris');
         });
     });
 });
