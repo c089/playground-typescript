25836df716e380c01ae82857e87b528cbb80f4df
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-11-21 09:16:53 +0100

    Remove code

--- spec.ts
+++
@@ -1,3 +0,0 @@
-describe('something', () => {
-    it('should pass', () => {});
-});
--- steps.ts
+++ steps.ts
@@ -1,42 +1,8 @@
 const {Given, When, Then} = require('cucumber');
 const assert = require('assert');
 
-type Amount = String;
-type DateWithoutTime = Date;
-
-class Account {
-    deposit(amount: Amount) {
-    }
-
-    withdraw(amount: Amount) {
-    }
-
-    printStatement() {
-        return (
-            '24.12.2015   +500      500' + '\n' +
-            '23.8.2016    -100      400'
-        );
-    }
-};
-
-let account: Account;
-let statement;
-Given('I have opened an account', function () {
-    account = new Account();
+Given('something', function () {
 });
 
-Given('I have deposited {int} EUR on {string}', function (amountDeposited, onDate) {
-    account.deposit(amountDeposited);
-});
-
-Given('I have withdrawn {int} EUR on {string}', function (amountWithdrawn, onDate) {
-    account.withdraw(amountWithdrawn);
-});
-
-When('I view my statement', function () {
-    statement = account.printStatement();
-});
-
-Then('the statement should read:', function (expectedStatement) {
-    assert.equal(statement, expectedStatement);
+When('something with a {string}', function (theString) {
 });
