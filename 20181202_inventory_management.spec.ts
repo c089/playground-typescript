5234935cea7da2ec75c24ca16541ca634a35f0cf
Christoph Neuroth <christoph.neuroth@gmail.com>
2018-12-05 12:44:38 +0100

    Day 2 part 2

--- 20181202_inventory_management.spec.ts
+++ 20181202_inventory_management.spec.ts
@@ -1,13 +1,16 @@
 import { groupBy } from 'fp-ts/lib/NonEmptyArray';
+import { zipWith } from 'fp-ts/lib/Array';
+import { left, right, isRight } from 'fp-ts/lib/Either';
+import { some, none } from 'fp-ts/lib/Option';
 
 const checksum = (boxIds => {
     const idsCountingForN = n => boxIds
         .filter(id => {
             const letters: Array<string> = id.split('');
             const groupedLettersByCount = groupBy(letters, letter =>
-                    letters.filter(l => letter == l).length.toString());
+                letters.filter(l => letter == l).length.toString());
             return groupedLettersByCount[n] !== undefined;
-    });
+        });
 
     return idsCountingForN(2).length * idsCountingForN(3).length;
 });
@@ -54,4 +57,85 @@ describe('Advent of Code 2018 Day 2: Inventory management', () => {
             'abcdee'
         ])).toEqual(12);
     });
+
+    describe('part 2', () => {
+        const commonCharacters = (id1, id2) => {
+            return zipWith(id1, id2, (a: string, b: string) => a == b ? some(a) : none)
+                .reduce((acc, maybeSame) => maybeSame.map(distinctPairs => acc + distinctPairs).getOrElse(acc), '');
+        };
+
+        const haveDifferenceOfOne = ([id1, id2]) => {
+            return zipWith(id1, id2, (a, b) => a == b ? left(a) : right([a, b]))
+                .filter(isRight).length === 1;
+        };
+
+        const distinctPairs = <A>(xs: Array<A>): Array<[A, A]> => {
+            const result = [];
+            for (let i = 0; i < xs.length; i++) {
+                for (let j = i + 1; j < xs.length; j++) {
+                    result.push([xs[i], xs[j]]);
+                }
+            }
+            return result;
+        }
+
+        const filterByDifferenceOfOne = (input: Array<string>) => {
+            return distinctPairs(input)
+                .filter(haveDifferenceOfOne);
+        }
+
+        const puzzle = ids => {
+            return filterByDifferenceOfOne(ids).map(([a, b]) => commonCharacters(a, b))
+        };
+
+        describe('puzzle', () => {
+            it('solves the puzzle', () => {
+                expect(puzzle([
+                    'abcde',
+                    'fghij',
+                    'klmno',
+                    'pqrst',
+                    'fguij',
+                    'axcye',
+                    'wvxyz'
+                ])).toEqual(['fgij']);
+            });
+        });
+
+        describe('commonCharacters', () => {
+            it('works for two boxes with a distance of 1', () => {
+                expect(commonCharacters('fghij', 'fguij')).toEqual('fgij');
+            });
+
+            it('works for two boxes with a distance of 2', () => {
+                expect(commonCharacters('fghij', 'fguix')).toEqual('fgi');
+            });
+
+            it('works for two boxes no shared charactesr', () => {
+                expect(commonCharacters('abc', 'xyz')).toEqual('');
+            });
+        });
+
+        describe('filterByDifferenceOfOne', () => {
+
+            it('given two ids differing by one charater, returns both', () => {
+                expect(filterByDifferenceOfOne(['fghij', 'fguij'])).toEqual([['fghij', 'fguij']]);
+            });
+
+            it('given two ids differing by two characters, return neither', () => {
+                expect(filterByDifferenceOfOne(['fghij', 'fguix'])).toEqual([]);
+            });
+
+            it('distinctPairs shuold foo', () => {
+                expect(distinctPairs([1, 2, 3])).toEqual([[1, 2], [1, 3], [2, 3]]);
+            });
+
+            it('given two ids differing by one charater and another one, returns both', () => {
+                expect(filterByDifferenceOfOne(['fghij', 'vwxyz', 'fguij'])).toEqual([
+                    ['fghij', 'fguij']
+                ]);
+            });
+        });
+    });
+
 });
