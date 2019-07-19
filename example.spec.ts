2180155ca6d934bac9d575628bcc0b4d5eb86308
Christoph Neuroth <christoph@ausdertechnik.de>
2019-07-19 14:34:21 +0200

    Update fp-ts, removing incompatible aoc code

---
+++ example.spec.ts
@@ -0,0 +1,9 @@
+import * as Option from 'fp-ts/lib/option';
+
+describe('fp-ts', () => {
+    describe('Option', () => {
+        it('maps over the value', () => {
+            expect(Option.map((x: number) => x + 1)(Option.some(1))).toEqual(Option.some(2));
+        })
+    });
+});
